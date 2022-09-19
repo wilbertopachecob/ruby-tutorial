6df749e2f46d22882066739d332488813d3b8e83
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2022-09-18 22:08:36 -0500

    added file for is statements

---
+++ if_statement.rb
@@ -0,0 +1,10 @@
+is_tall = true
+is_man = true
+
+if is_tall && is_man
+    puts "You are a tall man" 
+elsif is_tall && !is_man 
+    puts "You are a tall person"
+else 
+    puts "Yuo are either not tall or not a man"    
+end
