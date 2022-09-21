7e7df8bf0c39324a6b3dabe7d707d94882e3a15e
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2022-09-20 23:14:11 -0500

    added new files

---
+++ for_loop.rb
@@ -0,0 +1,17 @@
+friends = ["Caleb", "Juan", "Pedro"]
+
+for friend in friends
+    puts friend
+end
+
+friends.each do |friend|
+    puts friend
+end
+
+for index in 0..5 
+    puts index
+end
+
+6.times do |index|
+    puts index
+end
---
+++ guessing_game.rb
@@ -0,0 +1,21 @@
+secret = "giraffe"
+guess = ""
+guess_limit = 3
+guess_count = 0
+out_of_guesses = false
+
+while guess != secret && !out_of_guesses
+    if guess_count < guess_limit 
+      puts "Enter guess:"
+      guess = gets.chomp()
+      guess_count += 1  
+    else
+      out_of_guesses = true          
+    end
+end
+
+if out_of_guesses
+    puts "You lose"
+else
+    puts "You won"    
+end
