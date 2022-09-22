1835301684ca92d71c3c8fe35b3c5e06bd436480
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2022-09-21 22:45:06 -0500

    all about writing files

---
+++ writing_files.rb
@@ -0,0 +1,14 @@
+# opens file to read and write
+# File.open('friends.txt', 'r+') do |file|
+#     file.readline()
+#     file.write('Hi') # this will overrride the second line
+# end
+
+# File.open('friends.txt', 'a') do |file|    
+#     file.write("\nOlga") # appends at the end of the file
+# end
+
+# writes to file. If not exisit it creates it
+File.open('index.html', 'w') do |file|
+    file.write("<h1>Hello World</h1>") # appends at the end of the file
+end
